a92ce0b513b08c3b92497f8761bfdfa0ef347d77
Leonardo <leonardo.bonetti@skipthedishes.ca>
2021-08-25 15:44:32 -0500

    improvements: Remove unnecessary routes

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,16 +1,13 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { useNavigationPresenter } from 'navigation/presenter';
 import React from 'react';
 
 import HomeScreen from 'screens/Home';
-import SplashScreen from 'screens/Splash';
 
 const Stack = createNativeStackNavigator();
 
 const RootNavigation = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Splash" component={SplashScreen} />
       <Stack.Screen name="Home" component={HomeScreen} />
     </Stack.Navigator>
   );
