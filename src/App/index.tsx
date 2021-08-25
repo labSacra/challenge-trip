c1079ff4eaea7448dc7e881b0f258eaac55c8cad
Leonardo <leonardo.bonetti@skipthedishes.ca>
2021-08-25 15:51:01 -0500

    improvements: Remove header from screen

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,24 +1,11 @@
 import { NavigationContainer } from '@react-navigation/native';
-import useAppPresenter from 'App/presenter';
 import RootNavigation from 'navigation';
 import React from 'react';
-import { ActivityIndicator, View } from 'react-native';
 
-const App = () => {
-  const { isReady } = useAppPresenter();
-
-  return (
-    <NavigationContainer>
-      {isReady ? (
-        <RootNavigation />
-      ) : (
-        <View
-          style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-          <ActivityIndicator size="large" />
-        </View>
-      )}
-    </NavigationContainer>
-  );
-};
+const App = () => (
+  <NavigationContainer>
+    <RootNavigation />
+  </NavigationContainer>
+);
 
 export default App;
