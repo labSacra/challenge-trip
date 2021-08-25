1506ff056f7eb9bdcf1d0c945b6b67bb98d3953f
Leonardo <leonardo.bonetti@skipthedishes.ca>
2021-08-24 21:17:32 -0500

    task-1: Update Home UI

--- src/components/buttons/index.tsx
+++ src/components/buttons/index.tsx
@@ -1,6 +1,25 @@
-import styled from 'styled-components/native';
+import React from 'react';
+import { Pressable, Text } from 'react-native';
 
-export const PrimaryButton = styled.Button`
-  padding: 100px;
-  background-color: black;
-`;
+type ButtonProps = {
+  onPress?: () => void;
+  title: string;
+};
+
+/**
+ * Not using styled components because pressable is not properly supported
+ */
+export const Button = ({ onPress, title }: ButtonProps) => (
+  <Pressable
+    onPress={onPress}
+    style={({ pressed }) => ({
+      backgroundColor: pressed ? '#d2f6f7' : '#afeef0',
+      padding: 10,
+      minWidth: 90,
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius: 10,
+    })}>
+    <Text>{title}</Text>
+  </Pressable>
+);
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components/native';
+
+export const WrapperView = styled.View`
+  flex-direction: row;
+  justify-content: space-around;
+  width: 100%;
+  margin: 20px;
+`;
+
+export const StyledImage = styled.Image.attrs(() => ({
+  resizeMode: 'contain',
+}))`
+  width: 314;
+  height: 226;
+`;
