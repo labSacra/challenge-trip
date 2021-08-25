bff0bfdc292f1406769748a4b3822c8047f58f3b
Leonardo <leonardo.bonetti@skipthedishes.ca>
2021-08-25 14:40:03 -0500

    task-4: Clean unused code

--- src/App/presenter.ts
+++ src/App/presenter.ts
@@ -1,27 +1,11 @@
-import { repoInitializationUseCase } from 'domain/initialization/useCases/repoInitializationUseCase';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 type AppPresenter = {
   isReady: boolean;
 };
 
 const useAppPresenter = () => {
-  const [isReady, setIsReady] = useState(false);
-
-  useEffect(() => {
-    const subscription = repoInitializationUseCase().subscribe({
-      error: err => console.log(`Error initializing Repos: ${err.message}`),
-      complete: () => {
-        console.log('Repo initialization successful');
-        setIsReady(true);
-      },
-    });
-
-    return () => {
-      subscription.unsubscribe();
-    };
-  }, []);
-
+  const [isReady, setIsReady] = useState(true);
   return {
     isReady,
   };
--- src/domain/initialization/useCases/repoInitializationUseCase.ts
+++
@@ -1,4 +0,0 @@
-import { defer, Observable } from 'rxjs';
-
-export const repoInitializationUseCase = (): Observable<void> =>
-  defer(async () => {});
